543884307823c4b544508e988a0b120f79f1b00b
Charlie <charlieforss@gmail.com>
2024-11-03 19:16:42 +0100

    panel

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
-public class Main {
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Collections;
+import javax.swing.*;
 
+public class Main extends JFrame implements ActionListener  {
+    Panel p = new Panel();
+    Panel p2 = new Panel();
+    Panel p3 = new Panel();
+
+    JButton button2 = new JButton("Start/Restart");
+    JButton button3 = new JButton("Exit");
+    JButton button4 = new JButton("CLICKA INTE");
+    JButton button5 = new JButton("");
+    ArrayList<JButton> buttons;
 
     public Main() {
 
@@ -11,5 +26,10 @@ public class Main {
 
     public static void main(String[] args) {
         Main main = new Main();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 }
